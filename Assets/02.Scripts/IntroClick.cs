9a22826b5f09e15806223ab738067505c51fa712
master <a01063173324@gmail.com>
2018-12-04 12:30:20 +0900

    first commit test

---
+++ Assets/02.Scripts/IntroClick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class IntroClick : MonoBehaviour {
+
+    private UISprite m_Fade;
+    public float m_fDuration = 3f;
+
+    void Start()
+    {
+        m_Fade = GameObject.Find("FadeObject").GetComponent<UISprite>();
+        StartCoroutine(FadeOut());
+    }
+
+    IEnumerator FadeOut()
+    {
+        TweenAlpha.Begin(m_Fade.gameObject,m_fDuration, 0f);
+        yield return new WaitForSeconds(m_fDuration);
+
+        TweenAlpha.Begin(m_Fade.gameObject,m_fDuration, 1f);
+        yield return new WaitForSeconds(m_fDuration);
+
+        NextSceneCall();
+    }
+
+    void NextSceneCall()
+    {
+        SceneManager.LoadScene("S_2_GameStart");
+    }
+}
---
+++ Assets/02.Scripts/s_2_startClick.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class s_2_startClick : MonoBehaviour {
+
+    private UISprite m_Fade;
+    public float m_fDuration = 3f;
+    private GameObject gobject;
+
+    private UISprite _sprite;
+
+    private void Awake()
+    {
+        _sprite = GetComponent<UISprite>();
+    }
+    private void Start()
+    {
+        gobject = GameObject.Find("FadeObject");
+    }
+    void OnClick()
+    {
+//        _sprite.color = new Color(Random.value, Random.value, Random.value);
+        gobject.SetActive(true);
+        m_Fade = GameObject.Find("FadeObject").GetComponent<UISprite>();
+        StartCoroutine(FadeOut());
+    }
+    IEnumerator FadeOut()
+    {
+        TweenAlpha.Begin(m_Fade.gameObject, m_fDuration, 1f);
+        yield return new WaitForSeconds(m_fDuration);
+
+        NextSceneCall();
+    }
+
+    void NextSceneCall()
+    {
+        SceneManager.LoadScene("S_3_Gallery");
+    }
+}
