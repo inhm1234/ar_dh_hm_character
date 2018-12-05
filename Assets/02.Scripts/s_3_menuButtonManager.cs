94cc3b55d83a046f0c623b62c5b400f0f9ddaf94
DongHun <yhsk1218@naver.com>
2018-12-05 22:44:39 +0900

    <S_3> - 메뉴 버튼 눌렀을 시 나오는 버튼 기능구현(현재 Log만 찍힘) - 메뉴백그라운드이미지변경, 버튼이미지추가, 아틀라스추가

---
+++ Assets/02.Scripts/s_3_menuButtonManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_3_menuButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Notice()
+    {
+        Debug.Log("Notice Active");
+    }
+    void Inquiry()
+    {
+        Debug.Log("Inquiry Active");
+    }
+    void Option()
+    {
+        Debug.Log("Option Active");
+    }
+    void Review()
+    {
+        Debug.Log("Review Active");
+    }
+    void Exit()
+    {
+        Debug.Log("Exit Active");
+    }
+}
