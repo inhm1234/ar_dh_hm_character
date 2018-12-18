7eb8d6209fa1945e37d7c5089c3db584d1dabb58
master <a01063173324@gmail.com>
2018-12-18 13:32:39 +0900

    s_3_gallery 이미지 변경 완료. JSON 저장 방식 확인.

--- Assets/02.Scripts/DataManager.cs
+++ Assets/02.Scripts/DataManager.cs
@@ -13,18 +13,42 @@ using LitJson;
 /* 플레이어 데이터 저장클래스 */
 public class PlayerInfo
 {
-    int test1;
-    int test2;
-    int test3;
-    int test4;
+    public string name_;
+    public int scores_;
+//    int test1;
+//    int test2;
+//    int test3;
+//    int test4;
 
-    public PlayerInfo(int a, int b, int c, int d)
+//    public PlayerInfo(int a, int b, int c, int d)
+//    {
+//        test1 = a;
+//        test2 = b;
+//        test3 = c;
+//        test4 = d;
+//    }
+public PlayerInfo(string name,int scores)
     {
-        test1 = a;
-        test2 = b;
-        test3 = c;
-        test4 = d;
+        name_ = name;
+        scores_ = scores;
     }
+    public void printNumber()
+    {
+        //        Debug.Log(test1);
+        //        Debug.Log(test2);
+        //        Debug.Log(test3);
+        //        Debug.Log(test4);
+
+        Debug.Log(name_);
+        Debug.Log(scores_);
+    }
+}
+[SerializeField]
+class Data
+{
+    public string name;
+    public List<string> likes;
+    public int level;
 }
 
 public class DataManager : MonoBehaviour {
@@ -49,19 +73,38 @@ public class DataManager : MonoBehaviour {
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+
+        Data data = new global::Data();
+        data.name = "형민";
+        data.level = 10;
+        data.likes = new List<string>()
+        {
+            "dog","cat"
+        };
+        Debug.Log(JsonUtility.ToJson(data, prettyPrint: true));
+        JsonData infoJson = JsonUtility.ToJson(data, prettyPrint: true);
+        File.WriteAllText(Application.dataPath + "/Resources/Data/PlayerInfoData.json", infoJson.ToString());
+
     }
 
     public void SavePlayerInfo()
     {
         Debug.Log("SavePlayerInfo");
-        playerInfoList.Add(new PlayerInfo(1, 2, 3, 4));
-        Debug.Log(playerInfoList);
+//        playerInfoList.Add(new PlayerInfo(1, 2, 3, 4));
+//        playerInfoList.Add()
 
-        JsonData infoJson = JsonMapper.ToJson(playerInfoList);
-
-        File.WriteAllText(Application.dataPath + "/Resources/Data/PlayerInfoData.json", infoJson.ToString());
+        for(int i = 0; i < playerInfoList.Count;i++)
+        {
+            JsonData infoJson = JsonMapper.ToJson(playerInfoList[i]);
+            Debug.Log(infoJson);
+            File.WriteAllText(Application.dataPath + "/Resources/Data/PlayerInfoData.json", infoJson.ToString());
+        }
 
         Debug.Log(playerInfoList.Count);
+        for(int i =0; i < playerInfoList.Count;i++)
+        {
+            playerInfoList[i].printNumber();
+        }
     }
 
     public void LoadPlayerInfo()
@@ -77,11 +120,16 @@ public class DataManager : MonoBehaviour {
             for (int i = 0; i < playerData.Count; i++)
             {
                 Debug.Log(playerData.Count);
-                Debug.Log(playerData[i]["test1"].ToString());
+//                Debug.Log(playerData[i]["test1"].ToString());
             }
         }
   
         else
             Debug.Log("파일이 존재하지 않습니다.");
+
+        for (int i = 0; i < playerInfoList.Count; i++)
+        {
+            playerInfoList[i].printNumber();
+        }
     }
 }
